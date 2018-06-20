325192dde2d1b38731db127353864a9764c8af42
René Klatt <rene.klatt@spryker.com>
2018-06-20 10:12:34 +0000

    core-4452 fixed codestyle

--- src/SprykerEco/Service/Tideways/Plugin/TidewaysMonitoringExtensionPlugin.php
+++ src/SprykerEco/Service/Tideways/Plugin/TidewaysMonitoringExtensionPlugin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Apache OSL-2
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
