53df9bd94d961a25f723e8a24649da68589dc466
René Klatt <rene.klatt@spryker.com>
2018-07-26 07:17:50 +0000

    core-4452 added test suite

---
+++ tests/SprykerEcoTest/Service/Tideways/_support/SprykerEcoPluginTester.php
@@ -0,0 +1,26 @@
+<?php
+namespace SprykerEcoTest\Zed\Tideways;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class SprykerEcoPluginTester extends \Codeception\Actor
+{
+    use _generated\SprykerEcoPluginTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+}
